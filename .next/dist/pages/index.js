a72e081873fc5600adec378ead21615e41492fd8
sretundijr <sretundijr@gmail.com>
2017-09-12 21:26:33 -0600

    travis ci

--- .next/dist/pages/index.js
+++ .next/dist/pages/index.js
@@ -10,14 +10,6 @@ var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _jsxFileName = "/Users/steve/Desktop/thinkfulProjects/final-capstone/pages/index.js?entry";
-
 exports.default = function () {
-  return _react2.default.createElement("div", {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 1
-    }
-  }, "Welcome to next.js!");
+  return _react2.default.createElement("div", null, "Welcome to next.js!");
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBOzs7Ozs7OztrQkFBZSxZQUFBO3lCQUFNLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxHQUFBLEVBQU4sQUFBTTtBQUFyQiIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvVXNlcnMvc3RldmUvRGVza3RvcC90aGlua2Z1bFByb2plY3RzL2ZpbmFsLWNhcHN0b25lIn0=
