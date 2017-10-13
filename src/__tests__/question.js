34dd9c9815bbe4801dc48fb6c86f212afc4d3784
sretundijr <sretundijr@gmail.com>
2017-10-13 16:16:07 -0600

    render next button test

--- src/__tests__/question.js
+++ src/__tests__/question.js
@@ -1,12 +1,21 @@
-/* global describe it */
+/* global describe it expect */
 
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Question from '../components/questionaire/question';
+import Question, { renderNextButton } from '../components/questionaire/question';
 
 describe('testing issue category component', () => {
   it('should render without crashing', () => {
     shallow(<Question />);
+  });
+
+  describe('#renderNextButton', () => {
+    it('returns an empty string when there is only one element in the list', () => {
+      const list = ['hello'];
+      const returnedList = list.map((item, index, arr) => renderNextButton(index, arr.length));
+      const shallowRender = shallow(<div>{returnedList}</div>);
+      expect(shallowRender.find('Link').length).toEqual(0);
+    });
   });
 });
