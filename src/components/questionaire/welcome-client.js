116d15f81e9567cfe4ba4ff8e4d0f09fb4773507
sretundijr <sretundijr@gmail.com>
2017-10-13 16:17:04 -0600

    changes to welcome client component

--- src/components/questionaire/welcome-client.js
+++ src/components/questionaire/welcome-client.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 import { flexContainer } from '../../styles/questionnaire/shared-styles';
 
-export default function () {
+export default function (props) {
   return (
     <div>
       <div className="flex-container">
-        <h3>Welcome [client name here]</h3>
+        <h3>Welcome {props.customerName}</h3>
       </div>
       <div className="flex-container">
         <h3>
-          At [shop name here] we value your time and appericate your business
+          At {props.shopName} we value your time and appericate your business
         </h3>
       </div>
       <div className="flex-container">
         <h3 className="content-container">
           The following is a brief a questionaire to help us better understand
           the issues that you are experiencing.  Please complete this questionaire
-          before your appointment on [appointment date here]
+          before your appointment on {props.appointmentDate}
         </h3>
       </div>
       <style jsx>{flexContainer}</style>
