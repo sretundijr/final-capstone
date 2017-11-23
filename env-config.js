21effdf666aa3c62cded2d5c44d17c78aacfba0c
sretundijr <sretundijr@gmail.com>
2017-11-22 20:50:37 -0700

    fix typo

--- env-config.js
+++ env-config.js
@@ -2,5 +2,5 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? 'https://stark-scrubland-67367.herokuapp.com/api' : 'https://localhost:8080/api',
+  'process.env.BACKEND_URL': prod ? 'https://stark-scrubland-67367.herokuapp.com/api' : 'http://localhost:8080/api',
 };
