bc4252d0caf076d0e8f751d59984ecce9b439c7c
sretundijr <sretundijr@gmail.com>
2017-11-22 20:51:00 -0700

    add webpack config for auth0

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+
+const webpack = require('webpack');
+
 module.exports = {
   exportPathMap() {
     return {
@@ -6,6 +9,17 @@ module.exports = {
       '/thank-you': { page: '/thank-you' },
       '/advisor-dashboard': { page: '/advisor-dashboard' },
       '/view-questionnaire': { page: '/view-questionnaire' },
+      'auth-callback': { page: './auth-callback' },
     };
   },
+  webpack: (cfg) => {
+    cfg.plugins.push(new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }));
+
+    return cfg;
+  },
 };
+
