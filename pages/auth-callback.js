fffeec09005792bc493bd445e45d3a8fcfa278fc
sretundijr <sretundijr@gmail.com>
2017-11-27 19:39:03 -0700

    Added some context to auth callback

--- pages/auth-callback.js
+++ pages/auth-callback.js
@@ -1,8 +1,7 @@
 
 import React from 'react';
 import Router from 'next/router';
-
-// import loading from './loading.svg';
+import Head from 'next/head';
 
 import { setAccessToken, getAccessToken } from '../src/components/auth-login';
 import { loginUser } from '../src/api/login';
@@ -12,32 +11,34 @@ export default class Callback extends React.Component {
     setAccessToken();
     await loginUser(getAccessToken())
       .then((res) => {
-        console.log(res);
         Router.push(`/advisor-dashboard?_id=${res._id}`);
       });
   }
 
   render() {
-    return null;
-    // return (
-    //   <div>
-    //     <img src={loading} alt="loading" />
-    //     <style jsx>{`
-    //     div {
-    //       position: 'absolute';
-    //       display: 'flex';
-    //       justifyContent: 'center';
-    //       height: '100vh';
-    //       width: '100vw';
-    //       top: 0;
-    //       bottom: 0;
-    //       left: 0;
-    //       right: 0;
-    //       backgroundColor: 'white';
-    //     }
-    //     `}
-    //     </style>
-    //   </div>
-    // );
+    return (
+      <div>
+        <Head>
+          <title>Loading</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <div className="content-container">
+          <p>Loading...</p>
+          <style jsx>{`
+          .content-container {
+            text-align: center;
+            margin-top: 50vh;
+            font-size: 3vw;
+          }
+          @media only screen and (max-width: 450px) {
+            .content-container {
+              font-size: 6.0vw;
+            }
+          }
+        `}
+          </style>
+        </div>
+      </div>
+    );
   }
 }
