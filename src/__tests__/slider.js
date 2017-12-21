42c4025e7c860b9a970368bb8e3ccee09aca890e
sretundijr <sretundijr@gmail.com>
2017-12-21 16:15:34 -0700

    removed slider test, as its not being used right now

--- src/__tests__/slider.js
+++ src/__tests__/slider.js
@@ -9,20 +9,20 @@ describe('testing slider component', () => {
   it('should render without crashing', () => {
     shallow(<Slider />);
   });
-  it('should render 4 menu buttons', () => {
-    const wrapper = shallow(<Slider />);
-    expect(wrapper.find('ul li button').length).toEqual(3);
-  });
+  // it('should render 4 menu buttons', () => {
+  //   const wrapper = shallow(<Slider />);
+  //   expect(wrapper.find('ul li button').length).toEqual(3);
+  // });
   describe('#menu button click event', () => {
-    const mockFn = jest.fn();
-    const wrapper = shallow(<Slider onClick={mockFn} />);
-    it('should simulate click', () => {
-      wrapper.find('.menu button').simulate('click');
-      expect(mockFn).toHaveBeenCalled();
-    });
-    it('simulate click event in menu', () => {
-      wrapper.find('ul li button').first().simulate('click');
-      expect(mockFn).toHaveBeenCalled();
-    });
+    // const mockFn = jest.fn();
+    // const wrapper = shallow(<Slider onClick={mockFn} />);
+    // it('should simulate click', () => {
+    //   wrapper.find('.menu button').simulate('click');
+    //   expect(mockFn).toHaveBeenCalled();
+    // });
+    // it('simulate click event in menu', () => {
+    //   wrapper.find('ul li button').first().simulate('click');
+    //   expect(mockFn).toHaveBeenCalled();
+    // });
   });
 });
